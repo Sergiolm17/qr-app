cab21402c78676f3c8f4f90850f383c39f2c71f2
Sergio Lazaro Mondargo <sergiolazaromondargo@gmail.com>
2023-09-11 14:03:59 -0500

    qrGenerator

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,13 +2,13 @@
 
 import { useQRCode } from "next-qrcode";
 
-export default function Home() {
+export default function Home({ params }: { params: { slug: string } }) {
   const { Canvas } = useQRCode();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <Canvas
-        text={"https://github.com/bunlong/next-qrcode"}
+        text={decodeURIComponent(params.slug)}
         options={{
           errorCorrectionLevel: "M",
           margin: 3,
